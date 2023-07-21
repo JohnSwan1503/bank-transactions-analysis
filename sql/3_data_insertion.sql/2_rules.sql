1dc319ed7d2a9804915fd39f884b30673cc89a75
JohnSwan1503 <82048481+JohnSwan1503@users.noreply.github.com>
2023-07-21 14:20:08 -0700

    tabs to spaces

--- sql/3_data_insertion.sql/2_rules.sql
+++ sql/3_data_insertion.sql/2_rules.sql
@@ -1,15 +1,15 @@
 
 SELECT upsert_rules( 2020
-									 , 'personal'::accounttype
-									 , 'balance'::ruletype
-									 , ( 200.00 , 50.00::money ) );
+                   , 'personal'::accounttype
+                   , 'balance'::ruletype
+                   , ( 200.00 , 50.00::money ) );
 
 SELECT upsert_rules( 2020
-									 , 'business'::accounttype
-									 , 'activity_min'::ruletype
-									 , ( 100.00 , 100.00::money ) );
-									 
+                   , 'business'::accounttype
+                   , 'activity_min'::ruletype
+                   , ( 100.00 , 100.00::money ) );
+                   
 SELECT upsert_rules( 2020
-									 , 'business'::accounttype
-									 , 'activity_max'::ruletype
-									 , ( 100.00 , 100.00::money ) );
+                   , 'business'::accounttype
+                   , 'activity_max'::ruletype
+                   , ( 100.00 , 100.00::money ) );
